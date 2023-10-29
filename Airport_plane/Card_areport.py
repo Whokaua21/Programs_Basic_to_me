90f251d5d0890c54d4dd83fc406b4c2fbc2610ce
Klaus_mix <139010512+Whokaua21@users.noreply.github.com>
2023-10-29 07:48:44 -0300

    Upgrade

--- Airport_plane/Card_areport.py
+++ Airport_plane/Card_areport.py
@@ -1,8 +1,10 @@
+# Function to Register of Emails
 def Accont_plane():
     List_Emails = ['@GMAIL.COM','@LIVE.COM','@OUTLOK.COM']
     Dict_register = {}
-    print('-----CADASTRO DO CARTAO-----')
+    # Restart to Register
     while True:
+        print('-----CADASTRO DO CARTAO-----')
         Register_email = str(input('EMAIL:')).upper()
         Register_password = str(input('SENHA:'))
         if List_Emails[0] in Register_email or List_Emails[1] in Register_email or List_Emails[2] in Register_email :
@@ -11,8 +13,12 @@ def Accont_plane():
                 break
             print('Sua senha e muito curta por favor troque')
             continue
+        if Register_email in Dict_register.keys() and Register_password in Dict_register.values:
+            print('Sim')
         else:
             print('Ops Ocorreu um erro:')
             continue
     return Dict_register
-Accont_plane()
+
+
+
--- Airport_plane/Ticket.py
+++ Airport_plane/Ticket.py
@@ -1,38 +1,49 @@
+
 from random import randint as Rep_t
-def Pay_Ticket(Choice_place:str,Values_ticket:dict):
+from Card_areport import *
+Dict_to_play = {} 
+    # That function comfirm the pay: 
+def Pay_Ticket(Choice_place:str,Values_ticket:dict,Dicionario:dict ):
+    Dict_to_Accont = {}
     print(f'Você deseja Viajar para {Choice_place}')
     print(f'O valor da passagem e {Values_ticket}')
-    How_pay = input('''Para pagar você tem que criar um cartao, 
-        Você tem o cartao ?
-        [S/N]->''')
-    return How_pay
-
-# That function comfirm the pay: 
-
-
-print('Aeroporto do Brasil:')
-
-Dict_Place = {'CANADA':5.747,'ALEMANHA':5.518,
-              'JAPAO':9.230,'REINO UNIDO': 6.744,
-                'PORTUGAL':4.959,'RUSSIA':3.168,
-                'ANGOLA':6.043,'AFRICA':8.069,
-                'ITALIA':5.693,'MEXICO':4.731,
-                'GRECIA':6.478,'COREIA DO SUL':5.994}
-print(f'''___________COMPAINHA AVOAR DO CEU:PASSAGENS___________ 
-Canada:{Dict_Place['CANADA']}        Alemanha:{Dict_Place['ALEMANHA']}      Mexico:{Dict_Place['MEXICO']}
-Japao:{Dict_Place["JAPAO"]}         Russia:{Dict_Place['RUSSIA']}        Coreia do Sul:{Dict_Place['COREIA DO SUL']}
-Portugal:{Dict_Place["PORTUGAL"]}     Reino Unido:{Dict_Place['REINO UNIDO']}    Italia:{Dict_Place['ITALIA']}    
-Angola:{Dict_Place['ANGOLA']}         Africa:{Dict_Place['AFRICA']}        Grecia:{Dict_Place['GRECIA']}
-_______________________________________________________''')
-# The Chois_a_ticket is where client  speak where he what go
-try:
+    How_pay = str(input('''Para pagar você tem que criar um cartao, 
+            Você tem o cartao ?
+            [S/N]->''')).upper()
+    if How_pay == 'S':
+            Accont_plane()
+            if Accont_plane() == Pay_Ticket():
+                print('ep')
+    elif How_pay == 'N':
+            print('Não')
+            Dict_to_Accont = Accont_plane()
+            print(Dict_to_Accont)
+            return Dict_to_Accont
+while True:
+    Num = 0
+    #Here is a menu to client
+    print('Aeroporto do Brasil:')
+    Dict_Place = {'CANADA':5.747,'ALEMANHA':5.518,
+                'JAPAO':9.230,'REINO UNIDO': 6.744,
+                    'PORTUGAL':4.959,'RUSSIA':3.168,
+                    'ANGOLA':6.043,'AFRICA':8.069,
+                    'ITALIA':5.693,'MEXICO':4.731,
+                    'GRECIA':6.478,'COREIA DO SUL':5.994}
+    print(f'''___________COMPAINHA AVOAR DO CEU:PASSAGENS___________ 
+    Canada:{Dict_Place['CANADA']}        Alemanha:{Dict_Place['ALEMANHA']}      Mexico:{Dict_Place['MEXICO']}
+    Japao:{Dict_Place["JAPAO"]}         Russia:{Dict_Place['RUSSIA']}        Coreia do Sul:{Dict_Place['COREIA DO SUL']}
+    Portugal:{Dict_Place["PORTUGAL"]}     Reino Unido:{Dict_Place['REINO UNIDO']}    Italia:{Dict_Place['ITALIA']}    
+    Angola:{Dict_Place['ANGOLA']}         Africa:{Dict_Place['AFRICA']}        Grecia:{Dict_Place['GRECIA']}
+    _______________________________________________________''')
+    # The Chois_a_ticket is where client  speak where he what go
+    Num += 1
     Choice_a_ticket = str(input('Onde deseja Viajar:')).upper()
     if Choice_a_ticket in Dict_Place:
         Values_to_function = Dict_Place[Choice_a_ticket]
-        Pay_Ticket(Choice_a_ticket,Values_to_function)
-    elif Choice_a_ticket == str:
-        print('Erro:Tenha certeza que não escreveu nada errado')
-except:
-    print(''':Ocorreu um erro:
-Parece que você escreveu numero invés do nome do Pais''')
-# Here  he send to function
+        Dict_to_play = Pay_Ticket(Choice_a_ticket,Values_to_function,Dict_to_play)
+    elif Choice_a_ticket not in Dict_Place:
+        print('Erro:Certifique que o nome do pais esteja certo')
+    elif Num == 1:
+         Pay_Ticket(Choice_a_ticket,Values_to_function,Dict_to_play)
+    # Here  he send to function
+   
